e2fae06ae7ba091d4c5454495300d8f05f70a45d
shubhamnrt <shubhamnrt@gmail.com>
2020-12-18 17:02:21 +0530

    added thirsty

--- hungry.cpp
+++ hungry.cpp
@@ -2,14 +2,22 @@
 using namespace std;
 
 int main(){
-	string inp;
-	cin>>inp;
-	if(inp=="yes"){
+	string hungry;
+	cout<<"are you hungry "; 
+	cin>>hungry;
+	if(hungry=="yes"){
 		cout<<"eat samosa"<<endl;
 		cout<<"eat pizza"<<endl;
 		cout<<"eat burger"<<endl;
 	}
-	else cout<<"do your homework"<<endl;
-
+	else{
+		cout<<"are you thirsty ";
+		string thirsty;
+		cin>>thirsty;
+		if(thirsty=="yes"){
+			cout<<"drink water"<<endl;
+			cout<<"drink soda"<<endl;
+		}
+	}
 	return 0;
 }
