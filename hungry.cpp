5053df28efa7640f9115cb5417ea3bffbdfd9cfd
shubhamnrt <shubhamnrt@gmail.com>
2020-12-18 16:42:02 +0530

    added hungry file

---
+++ hungry.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string inp;
+	cin>>inp;
+	if(inp=="yes"){
+		cout<<"eat samosa"<<endl;
+		cout<<"eat pizza"<<endl;
+		cout<<"eat burger"<<endl;
+	}
+	else cout<<"do your homework"<<endl;
+
+	return 0;
+}
